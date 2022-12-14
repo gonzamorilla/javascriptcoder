dea821dd09980750e0706936fe1943ff8e7211d9
Gonzalo Morilla <gmorilla97@gmail.com>
2022-12-13 21:31:07 -0300

    Avance

--- js/script.js
+++ js/script.js
@@ -7,25 +7,42 @@ let nombreUsuario = prompt("Ingrese un nombre: ")
 let apellidoUsuario = prompt("Ingrese un apellido: ")
 let edadUsuario = parseInt(prompt("Ingrese su edad: "))
 
+const tiposDePorsche = [
+    {
+        nombre: 'Porsche Cayenne Turbo GT',
+        categoria:'suv',
+        precio: 237730
+    },
+    {
+        nombre: 'Porsche 911 GT2 RS',
+        categoria: 'circuito',
+        precio: 326930
+    },
+    {
+        nombre: 'Porsche 928 GTS',
+        categoria: 'daily',
+        precio: 93120
+    }
+]
+
 
 if (edadUsuario > 18) {
     alert("Bienvenido " + nombreUsuario + " " + apellidoUsuario)
 
-    do {
-        const grupo = Number(prompt("Seleccione el grupo que desea ver la tabla \n Grupo A - Digite 1 \n Grupo B - Digite 2 \n Grupo C - Digite 3 \n Grupo D - Digite 4"))
-        if (grupo == 1) {
-            alert("Holanda 7 pts \nSenegal 6 pts \nEcuador 4 pts \nQatar 2 pts")
-        } else if (grupo == 2) {
-            alert("Inglaterra 4 pts\nIrán 3 pts\nEstados Unidos 2 pts\nGales 1 pt")
-        } else if (grupo == 3) {
-            alert("Polonia 4 pts\nArgentina 3pts\nArabia Saudita 3 pts\nMexico 1pt")
-        } else if (grupo == 4) {
-            alert("Francia 6 pts\nAustralia 3pts\nDinamarca 1 pts\nTunez 1pt")
-        } else {
-            alert("Introduce una opción válida")
-        }
-    } while (grupo = Number);
-
 } else {
     alert("Debes ser mayor de edad para poder ingresar")
+
 }
+
+let categoriaSeleccionadaSuv = prompt("Escriba SUV para ver más información sobre los modelos disponibles").toLowerCase()
+let categoriaSUV = tiposDePorsche.find(tipo => tipo.categoria == "suv") 
+alert(categoriaSUV.nombre + "\nPrecio " + categoriaSUV.precio + " USD")
+
+let categoriaSeleccionadaCircuito = prompt("Escriba Circuito para ver más información sobre los modelos disponibles").toLowerCase()
+let categoriaCircuito = tiposDePorsche.find(tipo => tipo.categoria == "circuito") 
+alert(categoriaCircuito.nombre + "\nPrecio " + categoriaCircuito.precio + " USD")
+
+let categoriaSeleccionadaDaily = prompt("Escriba Daily para ver más información sobre los modelos disponibles").toLowerCase()
+let categoriaDaily = tiposDePorsche.find(tipo => tipo.categoria == "daily") 
+alert(categoriaDaily.nombre + "\nPrecio " + categoriaDaily.precio + " USD")
+
